3f70556d320711bf67a4eec232bf3968bc620403
kimpra2989 <106394183+kimpra2989@users.noreply.github.com>
2025-02-10 18:38:35 +0900

    Time: 13 ms (32.96%), Space: 54.1 MB (19.97%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    const hash = {}
+
+    for (const num of nums) {
+        if (hash[num]) delete hash[num]
+        else hash[num] = true
+    }
+
+    return +Object.keys(hash)
+};
