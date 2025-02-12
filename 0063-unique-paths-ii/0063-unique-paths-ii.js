9bb5c59b7099842bf0bf14da55061267d70a5413
kimpra2989 <106394183+kimpra2989@users.noreply.github.com>
2025-02-12 18:25:58 +0900

    Time: 26 ms (6.01%), Space: 53.3 MB (8.48%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function (obstacleGrid) {
+    const dp = []
+    const col_len = obstacleGrid[0].length
+    const firstRow = new Array(col_len + 1).fill(0)
+    dp.push(firstRow)
+
+    for (let r = 1; r <= obstacleGrid.length; r++) {
+        const row = [0, ...new Array(obstacleGrid[0].length).fill(0)]
+        if (r == 1) row[0] = 1
+        for (let c = 1; c <= col_len; c++) {
+            if (obstacleGrid[r - 1][c - 1] === 1) continue
+
+            row[c] = row[c - 1] + dp.at(-1)[c]
+        }
+        dp.push(row)
+    }
+    console.log(dp)
+    return dp.at(-1).at(-1)
+};
