2435478b4181207c7e15a23994d0f505714c50b7
kimpra2989 <106394183+kimpra2989@users.noreply.github.com>
2025-02-19 15:35:21 +0900

    Time: 27 ms (98.3%), Space: 69.4 MB (5.09%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.js
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.js
@@ -1,6 +1,8 @@
 
 var RecentCounter = function() {
-    this.req = []
+    this.q = {}
+    this.head = 1
+    this.tail = 1
 };
 
 /** 
@@ -8,13 +10,13 @@ var RecentCounter = function() {
  * @return {number}
  */
 RecentCounter.prototype.ping = function(t) {
-    this.req.push(t)
-    let count = 1
-    for (let i = this.req.length - 2; i >= 0; i--) {
-        if (this.req[i] < t - 3000) break
-        count++
+    this.q[this.tail] = t
+    this.tail++
+
+    while (this.q[this.head] < t - 3000) {
+        this.head++
     }
-    return count
+    return this.tail - this.head
 };
 
 /** 
