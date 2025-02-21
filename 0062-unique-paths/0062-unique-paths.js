a8d64018d55105cd3fd6958736f051ee6af918e8
kimpra2989 <106394183+kimpra2989@users.noreply.github.com>
2025-02-22 02:06:22 +0900

    Time: 15 ms (5.14%), Space: 58.5 MB (5.08%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+    const map = []
+    for (let r = 1; r <= m; r++) {
+        const row = new Array(n).fill(0)
+        map.push(row)
+    }
+
+    map[0][0] = 1
+
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            if (r == 0 && c == 0) continue
+            map[r][c] = (map[r][c - 1] ?? 0) + (map[r - 1]?.[c] ?? 0)
+        }
+    }
+    console.log(map)
+    return map.at(-1).at(-1)
+};
