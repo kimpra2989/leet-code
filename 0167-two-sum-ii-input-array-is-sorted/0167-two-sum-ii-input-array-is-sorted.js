1eca585017b917959ef7622ae7105bf8b2d70dd0
kimpra2989 <106394183+kimpra2989@users.noreply.github.com>
2025-05-10 01:10:52 +0900

    Time: 400 ms (6.73%), Space: 56.3 MB (51.37%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      const sum = numbers[i] + numbers[j]
+      if (sum === target) return [i + 1, j + 1]
+      else if (sum > target) break
+    }
+  }
+};
